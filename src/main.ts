bbc4d403d7ca881e1d161b1349833ae7f85efa0b
Wils0nDevVC <wilsonvc.developer@gmail.com>
2025-01-06 19:22:33 -0500

    ajuste comentario

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
 
   const logger = new Logger('Main ApiGateway')
   const app = await NestFactory.create(AppModule);
+  //*setGlobalPrefix : para que nuestros endpoint empiecen www.dominio.com/api/...
   app.setGlobalPrefix('api') //Esto es para que todos los endpoint empicen con la palabra "api"
   app.useGlobalPipes(
     new ValidationPipe({
@@ -19,7 +20,6 @@ async function bootstrap() {
    //*Aqui configuramos como un un filtro global nuestra instancia de RpcCustomExceptionFilter, para que podamos usarlo en todos nuestras modulos
    app.useGlobalFilters(new RpcCustomExceptionFilter())
    
-   //*setGlobalPrefix : para que nuestros endpoint empiecen www.dominio.com/api/...
   await app.listen(envs.port);
 
   logger.log(`Gateway corriendo en el puerto ${envs.port}`)
